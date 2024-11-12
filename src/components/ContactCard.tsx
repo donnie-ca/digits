dace4b3d39ebc16cc006c5e32f5bda28b0c23ed4
dcanaday <dlccanaday@gmail.com>
2024-11-11 21:44:32 -1000

    I have DNF'd this assignment but this commit is so I can do E54

--- src/components/ContactCard.tsx
+++ src/components/ContactCard.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import {Contact} from '@/lib/validationSchemas';
+import {Contact} from '@prisma/client';
+import Link from 'next/link'
 //import { Stuff } from '@prisma/client';
 //import Link from 'next/link';
 import {Card, Image} from 'react-bootstrap';
@@ -10,12 +11,15 @@ const ContactCard = ({ contact }: {contact: Contact}) => (
   <Card className='h-100'>
     <Card.Header>
         <Image src={contact.image} width='75' height='75'/>
-        <Card.Title> {contact.firstName}  &nbsp: {contact.lastName} </Card.Title>
+        <Card.Title> {contact.firstName} {contact.lastName} </Card.Title>
         <Card.Subtitle> {contact.address} </Card.Subtitle>
     </Card.Header>
     <Card.Body> 
         <Card.Text> {contact.description} </Card.Text>
     </Card.Body>
+    <Card.Footer>
+      <Link href={`edit/${contact.id}`}>Edit</Link>
+    </Card.Footer>
   </Card>
 );
 
--- src/components/ContactCardAdmin.tsx
+++ src/components/ContactCardAdmin.tsx
@@ -10,7 +10,7 @@ const ContactCardAdmin = ({ contact }: {contact: Contact}) => (
   <Card className='h-100'>
     <Card.Header>
         <Image src={contact.image} width='75' height='75'/>
-        <Card.Title> {contact.firstName}  &nbsp: {contact.lastName} </Card.Title>
+        <Card.Title> {contact.firstName} {contact.lastName} </Card.Title>
         <Card.Subtitle> {contact.address} </Card.Subtitle>
     </Card.Header>
     <Card.Body> 
